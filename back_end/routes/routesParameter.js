397a3c4b130e509fef977f7c478828452ee92820
Phat7203 <trantienphat.mx@gmail.com>
2025-05-25 14:13:45 +0700

    fix: remove back_end

--- back_end/routes/routesParameter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const parameterController = require('../controllers/parameterController');
-
-router.get('/get', verifyToken , parameterController.getParameter);
-
-router.put('/update', verifyToken , parameterController.updateParameter);
-
-module.exports = router;
