1c4b5e5da297d7de7d3f5369e6bcca1086b56284
Phat7203 <trantienphat.mx@gmail.com>
2025-05-25 11:23:51 +0700

    fix: Fix firebase config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPw4_SZc0gdWPxTpApjid0SrbbdRFnsRA",
-  authDomain: "shoppingapp-a20a4.firebaseapp.com",
-  projectId: "shoppingapp-a20a4",
-  storageBucket: "shoppingapp-a20a4.appspot.com",
-  messagingSenderId: "290796953622",
-  appId: "1:290796953622:web:19baaa4f754afa99a6d537",
-  measurementId: "G-GS65ZJ605C",
-  databaseURL: "https://shoppingapp-a20a4-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
